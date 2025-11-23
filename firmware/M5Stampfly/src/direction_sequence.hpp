a6e5035c40ab0b6abf68b117fe03d65fc531eb4e
cube-zz <znynpx617@gmail.com>
2025-11-23 13:14:01 +0900

    flipなし

--- firmware/M5Stampfly/src/direction_sequence.hpp
+++ firmware/M5Stampfly/src/direction_sequence.hpp
@@ -9,6 +9,6 @@ typedef enum {
     FLIP,
 } Direction_t;
 
-Direction_t direction_sequence[] = {RIGHT, FORWARD, FLIP};
+Direction_t direction_sequence[] = {RIGHT, FORWARD};
 
 uint8_t MAX_STATES_NUM = sizeof(direction_sequence) / sizeof(direction_sequence[0]);
